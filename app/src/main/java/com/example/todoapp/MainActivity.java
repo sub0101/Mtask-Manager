5579b2eb5371b746968ef18d5fca29a99ab956ee
sub0101 <surajbisht0101@gmail.com>
2023-05-28 19:02:32 +0530

    temp of drawer

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -9,8 +9,10 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
@@ -35,6 +37,7 @@ import com.example.todoapp.fragments.SettingFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -63,37 +66,50 @@ public class MainActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_main);
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        category_recycler = findViewById(R.id.category_container);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Mtask Manager");
-//        toolbar.setBackgroundColor(getResources().getColor(R.color.dark_slate_gray));
-        flag = true;
-        homeFragment = new HomeFragment();
-        settingFragment = new SettingFragment();
-        profileFragment = new PofileFragment();
-        currrent_fragment = homeFragment;
-        fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().add(R.id.container, homeFragment).commit();
-        fragmentManager.beginTransaction().add(R.id.container, settingFragment).hide(settingFragment).commit();
-        fragmentManager.beginTransaction().add(R.id.container, profileFragment).hide(profileFragment).commit();
-        bottomNavigation.setOnItemSelectedListener(new OnBottomNavigationItemListner());
-        bottomNavigation.setSelectedItemId(R.id.home);
-        categoryInfos = new ArrayList<>();
-        categoryInfos  = viewModel.getAllCategory().getValue();
-        CategoryAdapter categoryAdapter = new CategoryAdapter( categoryInfos);
-        category_recycler.setAdapter(categoryAdapter);
-        category_recycler.setLayoutManager(new LinearLayoutManager(this , LinearLayoutManager.HORIZONTAL , false));
-viewModel.getAllCategory().observe(this, new Observer<List<CategoryInfo>>() {
-    @Override
-    public void onChanged(List<CategoryInfo> categoryInfo) {
-        categoryInfos = categoryInfo;
-        categoryAdapter.updateSet(categoryInfos);
-        categoryAdapter.notifyItemInserted(categoryInfo.size());
+        getSupportActionBar().setTitle("ToolBar");
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation, R.string.navigation_c);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+//        navigationView.setNavigationItemSelectedListener(this);
+//
+//        navigationView.setCheckedItem(R.id.navigation);
 
-    }
-});
+//        bottomNavigation = findViewById(R.id.bottom_navigation);
+//        category_recycler = findViewById(R.id.category_container);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setTitle("Mtask Manager");
+////        toolbar.setBackgroundColor(getResources().getColor(R.color.dark_slate_gray));
+//        flag = true;
+//        homeFragment = new HomeFragment();
+//        settingFragment = new SettingFragment();
+//        profileFragment = new PofileFragment();
+//        currrent_fragment = homeFragment;
+//        fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction().add(R.id.container, homeFragment).commit();
+//        fragmentManager.beginTransaction().add(R.id.container, settingFragment).hide(settingFragment).commit();
+//        fragmentManager.beginTransaction().add(R.id.container, profileFragment).hide(profileFragment).commit();
+//        bottomNavigation.setOnItemSelectedListener(new OnBottomNavigationItemListner());
+//        bottomNavigation.setSelectedItemId(R.id.home);
+//        categoryInfos = new ArrayList<>();
+//        categoryInfos  = viewModel.getAllCategory().getValue();
+//        CategoryAdapter categoryAdapter = new CategoryAdapter( categoryInfos);
+//        category_recycler.setAdapter(categoryAdapter);
+//        category_recycler.setLayoutManager(new LinearLayoutManager(this , LinearLayoutManager.HORIZONTAL , false));
+//viewModel.getAllCategory().observe(this, new Observer<List<CategoryInfo>>() {
+//    @Override
+//    public void onChanged(List<CategoryInfo> categoryInfo) {
+//        categoryInfos = categoryInfo;
+//        categoryAdapter.updateSet(categoryInfos);
+//        categoryAdapter.notifyItemInserted(categoryInfo.size());
+//
+//    }
+//});
     }
 
 
@@ -111,76 +127,76 @@ viewModel.getAllCategory().observe(this, new Observer<List<CategoryInfo>>() {
 
     }
 
-    TaskViewModel getViewModel()
-    {
-        return viewModel;
-    }
-
-    private class OnBottomNavigationItemListner implements NavigationBarView.OnItemSelectedListener {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.home:
-                    System.out.println("home case");
-                    loadFragment(homeFragment);
-                    return true;
-
-                case R.id.setting:
-                    loadFragment(settingFragment);
-                    System.out.println("setting case");
-                    return true;
-                case R.id.profile:
-                    loadFragment(profileFragment);
-                    return true;
-            }
-            return true;
-        }
-
-    }
-
-    public void loadFragment(Fragment fragment) {
-        FragmentTransaction ft = fragmentManager.beginTransaction();
-        if (currrent_fragment != fragment) {
-            ft.hide(currrent_fragment).show(fragment).commit();
-            currrent_fragment = fragment;
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        System.out.println("on back pressed");
-
-        if (currrent_fragment != homeFragment) {
-            loadFragment(homeFragment);
-            bottomNavigation.setSelectedItemId(R.id.home);
-        } else {
-            super.onBackPressed();
-        }
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() ==(R.id.settingoption))
-        {
-            loadFragment(settingFragment);
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onDestroy() {
-//viewModel = PendingItemFragment.getViewModel();
-//viewModel.deleteAllTask();
-System.out.println("deleted all task");
-        super.onDestroy();
-    }
+//    TaskViewModel getViewModel()
+//    {
+//        return viewModel;
+//    }
+//
+//    private class OnBottomNavigationItemListner implements NavigationBarView.OnItemSelectedListener {
+//
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.home:
+//                    System.out.println("home case");
+//                    loadFragment(homeFragment);
+//                    return true;
+//
+//                case R.id.setting:
+//                    loadFragment(settingFragment);
+//                    System.out.println("setting case");
+//                    return true;
+//                case R.id.profile:
+//                    loadFragment(profileFragment);
+//                    return true;
+//            }
+//            return true;
+//        }
+//
+//    }
+//
+//    public void loadFragment(Fragment fragment) {
+//        FragmentTransaction ft = fragmentManager.beginTransaction();
+//        if (currrent_fragment != fragment) {
+//            ft.hide(currrent_fragment).show(fragment).commit();
+//            currrent_fragment = fragment;
+//        }
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        System.out.println("on back pressed");
+//
+//        if (currrent_fragment != homeFragment) {
+//            loadFragment(homeFragment);
+//            bottomNavigation.setSelectedItemId(R.id.home);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if(item.getItemId() ==(R.id.settingoption))
+//        {
+//            loadFragment(settingFragment);
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+////viewModel = PendingItemFragment.getViewModel();
+////viewModel.deleteAllTask();
+//System.out.println("deleted all task");
+//        super.onDestroy();
+//    }
 
 
 
